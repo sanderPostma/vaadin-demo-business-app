724a9a2f09994c9eceb63f78c47c796405997414
sanderPostma <spostma@sphereon.com>
2020-10-14 11:33:44 +0200

    Added project.basedir & vaadin.frontend.token.file init params

---
+++ src/main/resources/META-INF/resources/frontend/generated-flow-imports.js
@@ -0,0 +1,4 @@
+// Copied generated file here because of " ERROR [dev-webpack] (webpack) ERROR in Entry module not found: Error: Can't resolve 'C:\-DEV\Sandbox\vaadin-demo-business-app\target\classes\META-INF\resources\frontend\generated-flow-imports.js' in 'C:\-DEV\Sandbox\vaadin-demo-business-app\frontend'"
+const div = document.createElement('div');
+div.innerHTML = '<custom-style><style include="lumo-color lumo-typography"></style></custom-style>';
+document.head.insertBefore(div.firstElementChild, document.head.firstChild);
